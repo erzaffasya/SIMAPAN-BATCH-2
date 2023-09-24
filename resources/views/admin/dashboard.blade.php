d2d3ecd9e282c23cc3f653fb535a6b3e004971e7
Erza Fahmi Fasya <58721335+erzaffasya@users.noreply.github.com>
2023-09-24 12:20:20 +0800

    update statistik pengaduan

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,55 +1,11 @@
 <x-app-layout>
     <div class="content">
         <div class="row">
-            <div class="col-lg-3 col-sm-6 col-12">
-                <div class="dash-widget">
-                    <div class="dash-widgetimg">
-                        <span><img src="{{ asset('tadmin/assets/img/icons/dash1.svg') }}" alt="img"></span>
-                    </div>
-                    <div class="dash-widgetcontent">
-                        <h5>$<span class="counters" data-count="307144.00">$307,144.00</span></h5>
-                        <h6>Total Purchase Due</h6>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-3 col-sm-6 col-12">
-                <div class="dash-widget dash1">
-                    <div class="dash-widgetimg">
-                        <span><img src="{{ asset('tadmin/assets/img/icons/dash2.svg') }}" alt="img"></span>
-                    </div>
-                    <div class="dash-widgetcontent">
-                        <h5>$<span class="counters" data-count="4385.00">$4,385.00</span></h5>
-                        <h6>Total Sales Due</h6>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-3 col-sm-6 col-12">
-                <div class="dash-widget dash2">
-                    <div class="dash-widgetimg">
-                        <span><img src="{{ asset('tadmin/assets/img/icons/dash3.svg') }}" alt="img"></span>
-                    </div>
-                    <div class="dash-widgetcontent">
-                        <h5>$<span class="counters" data-count="385656.50">385,656.50</span></h5>
-                        <h6>Total Sale Amount</h6>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-3 col-sm-6 col-12">
-                <div class="dash-widget dash3">
-                    <div class="dash-widgetimg">
-                        <span><img src="{{ asset('tadmin/assets/img/icons/dash4.svg') }}" alt="img"></span>
-                    </div>
-                    <div class="dash-widgetcontent">
-                        <h5>$<span class="counters" data-count="40000.00">400.00</span></h5>
-                        <h6>Total Sale Amount</h6>
-                    </div>
-                </div>
-            </div>
             <div class="col-lg-3 col-sm-6 col-12 d-flex">
                 <div class="dash-count">
                     <div class="dash-counts">
-                        <h4>100</h4>
-                        <h5>Customers</h5>
+                        <h4>{{ $akun->count() }}</h4>
+                        <h5>Akun</h5>
                     </div>
                     <div class="dash-imgs">
                         <i data-feather="user"></i>
@@ -59,19 +15,19 @@
             <div class="col-lg-3 col-sm-6 col-12 d-flex">
                 <div class="dash-count das1">
                     <div class="dash-counts">
-                        <h4>100</h4>
-                        <h5>Suppliers</h5>
+                        <h4>{{ $jenisLayanan->count() }}</h4>
+                        <h5>Jenis Layanan</h5>
                     </div>
                     <div class="dash-imgs">
-                        <i data-feather="user-check"></i>
+                        <i data-feather="file-text"></i>
                     </div>
                 </div>
             </div>
             <div class="col-lg-3 col-sm-6 col-12 d-flex">
                 <div class="dash-count das2">
                     <div class="dash-counts">
-                        <h4>100</h4>
-                        <h5>Purchase Invoice</h5>
+                        <h4>{{ $jenisKekerasan->count() }}</h4>
+                        <h5>Jenis kekerasan</h5>
                     </div>
                     <div class="dash-imgs">
                         <i data-feather="file-text"></i>
@@ -81,8 +37,8 @@
             <div class="col-lg-3 col-sm-6 col-12 d-flex">
                 <div class="dash-count das3">
                     <div class="dash-counts">
-                        <h4>105</h4>
-                        <h5>Sales Invoice</h5>
+                        <h4>{{ $pengaduan }}</h4>
+                        <h5>Pengaduan</h5>
                     </div>
                     <div class="dash-imgs">
                         <i data-feather="file"></i>
@@ -93,24 +49,21 @@
         <!-- Button trigger modal -->
 
         <div class="row">
-            <div class="col-lg-7 col-sm-12 col-12 d-flex">
+            <div class="col-lg-12 col-sm-12 col-12 d-flex">
                 <div class="card flex-fill">
                     <div class="card-header pb-0 d-flex justify-content-between align-items-center">
-                        <h5 class="card-title mb-0">Purchase & Sales</h5>
-                        <div class="graph-sets">
+                        <h5 class="card-title mb-0">Pengaduan</h5>
+                        <div class="graph-sets mb-2">
                             <ul>
                                 <li>
-                                    <span>Sales</span>
-                                </li>
-                                <li>
-                                    <span>Purchase</span>
+                                    <span>Pengaduan</span>
                                 </li>
                             </ul>
-                            <div class="dropdown">
+                            <div class="dropdown mb-2">
                                 <button class="btn btn-white btn-sm dropdown-toggle" type="button"
                                     id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
-                                    2022 <img src="{{ asset('tadmin/assets/img/icons/dropdown.svg') }}"
-                                        alt="img" class="ms-2">
+                                    2022 <img src="{{ asset('tadmin/assets/img/icons/dropdown.svg') }}" alt="img"
+                                        class="ms-2">
                                 </button>
                                 <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
                                     <li>
@@ -128,167 +81,114 @@
 
                     </div>
                     <div class="card-body">
-                        <div id="sales_charts"></div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-5 col-sm-12 col-12 d-flex">
-                <div class="card flex-fill">
-                    <div class="card-header pb-0 d-flex justify-content-between align-items-center">
-                        <h4 class="card-title mb-0">Recently Added Products</h4>
-                        <div class="dropdown">
-                            <a href="javascript:void(0);" data-bs-toggle="dropdown" aria-expanded="false"
-                                class="dropset">
-                                <i class="fa fa-ellipsis-v"></i>
-                            </a>
-                            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                                <li>
-                                    <a href="productlist.html" class="dropdown-item">Product List</a>
-                                </li>
-                                <li>
-                                    <a href="addproduct.html" class="dropdown-item">Product Add</a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="card-body">
-                        <div class="table-responsive dataview">
-                            <table class="table datatable ">
-                                <thead>
-                                    <tr>
-                                        <th>Sno</th>
-                                        <th>Products</th>
-                                        <th>Price</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <td>1</td>
-                                        <td class="productimgname">
-                                            <a href="productlist.html" class="product-img">
-                                                <img src="{{ asset('tadmin/assets/img/product/product22.jpg') }}"
-                                                    alt="product">
-                                            </a>
-                                            <a href="productlist.html">Apple Earpods</a>
-                                        </td>
-                                        <td>$891.2</td>
-                                    </tr>
-                                    <tr>
-                                        <td>2</td>
-                                        <td class="productimgname">
-                                            <a href="productlist.html" class="product-img">
-                                                <img src="{{ asset('tadmin/assets/img/product/product23.jpg') }}"
-                                                    alt="product">
-                                            </a>
-                                            <a href="productlist.html">iPhone 11</a>
-                                        </td>
-                                        <td>$668.51</td>
-                                    </tr>
-                                    <tr>
-                                        <td>3</td>
-                                        <td class="productimgname">
-                                            <a href="productlist.html" class="product-img">
-                                                <img src="{{ asset('tadmin/assets/img/product/product24.jpg') }}"
-                                                    alt="product">
-                                            </a>
-                                            <a href="productlist.html">samsung</a>
-                                        </td>
-                                        <td>$522.29</td>
-                                    </tr>
-                                    <tr>
-                                        <td>4</td>
-                                        <td class="productimgname">
-                                            <a href="productlist.html" class="product-img">
-                                                <img src="{{ asset('tadmin/assets/img/product/product6.jpg') }}"
-                                                    alt="product">
-                                            </a>
-                                            <a href="productlist.html">Macbook Pro</a>
-                                        </td>
-                                        <td>$291.01</td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>
+                        <div id="s-line" class="chart-set"></div>
                     </div>
                 </div>
             </div>
         </div>
         <div class="card mb-0">
             <div class="card-body">
-                <h4 class="card-title">Expired Products</h4>
+                <h4 class="card-title">Recent Pengaduan</h4>
                 <div class="table-responsive dataview">
                     <table class="table datatable ">
                         <thead>
                             <tr>
-                                <th>SNo</th>
-                                <th>Product Code</th>
-                                <th>Product Name</th>
-                                <th>Brand Name</th>
-                                <th>Category Name</th>
-                                <th>Expiry Date</th>
+                                <th>No</th>
+                                <th>Nomor</th>
+                                <th>Tanggal</th>
+                                <th>Nama Korban</th>
+                                <th>KDRT/NON KDRT</th>
+                                <th>Status</th>
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <td>1</td>
-                                <td><a href="javascript:void(0);">IT0001</a></td>
-                                <td class="productimgname">
-                                    <a class="product-img" href="productlist.html">
-                                        <img src="{{ asset('tadmin/assets/img/product/product2.jpg') }}"
-                                            alt="product">
-                                    </a>
-                                    <a href="productlist.html">Orange</a>
-                                </td>
-                                <td>N/D</td>
-                                <td>Fruits</td>
-                                <td>12-12-2022</td>
-                            </tr>
-                            <tr>
-                                <td>2</td>
-                                <td><a href="javascript:void(0);">IT0002</a></td>
-                                <td class="productimgname">
-                                    <a class="product-img" href="productlist.html">
-                                        <img src="{{ asset('tadmin/assets/img/product/product3.jpg') }}"
-                                            alt="product">
-                                    </a>
-                                    <a href="productlist.html">Pineapple</a>
-                                </td>
-                                <td>N/D</td>
-                                <td>Fruits</td>
-                                <td>25-11-2022</td>
-                            </tr>
-                            <tr>
-                                <td>3</td>
-                                <td><a href="javascript:void(0);">IT0003</a></td>
-                                <td class="productimgname">
-                                    <a class="product-img" href="productlist.html">
-                                        <img src="{{ asset('tadmin/assets/img/product/product4.jpg') }}"
-                                            alt="product">
-                                    </a>
-                                    <a href="productlist.html">Stawberry</a>
-                                </td>
-                                <td>N/D</td>
-                                <td>Fruits</td>
-                                <td>19-11-2022</td>
-                            </tr>
-                            <tr>
-                                <td>4</td>
-                                <td><a href="javascript:void(0);">IT0004</a></td>
-                                <td class="productimgname">
-                                    <a class="product-img" href="productlist.html">
-                                        <img src="{{ asset('tadmin/assets/img/product/product5.jpg') }}"
-                                            alt="product">
-                                    </a>
-                                    <a href="productlist.html">Avocat</a>
-                                </td>
-                                <td>N/D</td>
-                                <td>Fruits</td>
-                                <td>20-11-2022</td>
-                            </tr>
+                            @foreach ($recentPengaduan as $item)
+                                <tr>
+                                    <td>{{$loop->iteration}}</td>
+                                    <td><a href="javascript:void(0);">{{$item->nomor}}</a></td>
+                                    <td class="productimgname">
+                                        {{$item->tanggal_registrasi}}
+                                    </td>
+                                    <td>{{$item->nama_korban}}</td>
+                                    <td>{{$item->kdrt_nonkdrt}}</td>
+                                    <td>{{$item->status}}</td>
+                                </tr>
+                            @endforeach
                         </tbody>
                     </table>
                 </div>
             </div>
         </div>
     </div>
+    @push('scripts')
+        <!-- Chart JS -->
+        <script src="{{ asset('tadmin/assets/plugins/apexchart/apexcharts.min.js') }}"></script>
+
+        <script>
+            $(document).ready(function() {
+
+                function generateData(baseval, count, yrange) {
+                    var i = 0;
+                    var series = [];
+                    while (i < count) {
+                        var x = Math.floor(Math.random() * (750 - 1 + 1)) + 1;;
+                        var y = Math.floor(Math.random() * (yrange.max - yrange.min + 1)) + yrange.min;
+                        var z = Math.floor(Math.random() * (75 - 15 + 1)) + 15;
+
+                        series.push([x, y, z]);
+                        baseval += 86400000;
+                        i++;
+                    }
+                    return series;
+                }
+
+                // Simple Line
+                if ($('#s-line').length > 0) {
+                    var sline = {
+                        chart: {
+                            height: 350,
+                            type: 'line',
+                            zoom: {
+                                enabled: false
+                            },
+                            toolbar: {
+                                show: false,
+                            }
+                        },
+                        // colors: ['#4361ee'],
+                        dataLabels: {
+                            enabled: false
+                        },
+                        stroke: {
+                            curve: 'straight'
+                        },
+                        series: [{
+                            name: "Pengaduan",
+                            data: [{{ $arrayStatistikPengaduan }}]
+                        }],
+                        grid: {
+                            row: {
+                                colors: ['#f1f2f3',
+                                    'transparent'
+                                ], // takes an array which will be repeated on columns
+                                opacity: 0.5
+                            },
+                        },
+                        xaxis: {
+                            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt',
+                                'Nov', 'Des'
+                            ],
+                        }
+                    }
+
+                    var chart = new ApexCharts(
+                        document.querySelector("#s-line"),
+                        sline
+                    );
+
+                    chart.render();
+                }
+            });
+        </script>
+    @endpush
 </x-app-layout>
