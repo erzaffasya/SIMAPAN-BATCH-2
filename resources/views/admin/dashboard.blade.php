dd09173d3043c38ca3d678563180c872fd63ff6c
Erza Fahmi Fasya <58721335+erzaffasya@users.noreply.github.com>
2023-10-22 17:26:06 +0800

    Update dashboard.blade.php

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -121,7 +121,7 @@
             </div>
         </div>
         <div class="row">
-            <div class="col-lg-6 col-sm-6 col-6 d-flex">
+            <div class="col-lg-12 col-sm-12 col-12 d-flex">
                 <div class="card flex-fill">
                     <div class="card-header pb-0 d-flex justify-content-between align-items-center">
                         <h5 class="card-title mb-0">Grafik Pengaduan</h5>
@@ -157,43 +157,7 @@
                     </div>
                 </div>
             </div>
-
-            <div class="col-lg-6 col-sm-6 col-6 d-flex">
-                <div class="card flex-fill">
-                    <div class="card-header pb-0 d-flex justify-content-between align-items-center">
-                        <h5 class="card-title mb-0">Grafik Pengaduan</h5>
-                        <div class="graph-sets mb-2">
-                            <ul>
-                                <li>
-                                    <span>Pengaduan</span>
-                                </li>
-                            </ul>
-                            <div class="dropdown mb-2">
-                                {{-- <button class="btn btn-white btn-sm dropdown-toggle" type="button"
-                                    id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
-                                    2022 <img src="{{ asset('tadmin/assets/img/icons/dropdown.svg') }}" alt="img"
-                                        class="ms-2">
-                                </button> --}}
-                                <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                                    <li>
-                                        <a href="javascript:void(0);" class="dropdown-item">2022</a>
-                                    </li>
-                                    <li>
-                                        <a href="javascript:void(0);" class="dropdown-item">2021</a>
-                                    </li>
-                                    <li>
-                                        <a href="javascript:void(0);" class="dropdown-item">2020</a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-
-                    </div>
-                    <div class="card-body">
-                        <div id="s-line" class="chart-set"></div>
-                    </div>
-                </div>
-            </div>
+            
         </div>
         <div class="card mb-0">
             <div class="card-body">
